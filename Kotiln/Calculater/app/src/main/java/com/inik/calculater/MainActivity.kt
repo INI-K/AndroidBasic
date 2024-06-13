b91cdfb0ed20dff53575f842603a4f768ffc438d
Kim Tae Hwan <annette57@naver.com>
2024-06-13 23:07:43 +0900

    Calculator -Decimal 처리 -0 오류 처리

--- Kotiln/Calculater/app/src/main/java/com/inik/calculater/MainActivity.kt
+++ Kotiln/Calculater/app/src/main/java/com/inik/calculater/MainActivity.kt
@@ -8,12 +8,14 @@ import android.widget.Button
 import android.widget.Toast
 import com.inik.calculater.databinding.ActivityMainBinding
 import java.io.File
+import java.text.DecimalFormat
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val firstNumberText = StringBuilder("")
     private val secondNumberText = StringBuilder("")
     private val operatorText = StringBuilder("")
+    private val decimalFormat = DecimalFormat("#,###")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -47,8 +49,8 @@ class MainActivity : AppCompatActivity() {
         val secondNumber = secondNumberText.toString().toBigDecimal()
 
         val result = when(operatorText.toString()){
-            "+" -> firstNumber + secondNumber
-            "-" -> firstNumber - secondNumber
+            "+" -> decimalFormat.format(firstNumber + secondNumber)
+            "-" -> decimalFormat.format(firstNumber - secondNumber)
             else ->
                 "잘못된 수식입니다."
         }.toString()
@@ -72,6 +74,9 @@ class MainActivity : AppCompatActivity() {
         updateEquationTextView()
     }
     private fun updateEquationTextView(){
-        binding.equationTextView.text = "$firstNumberText $operatorText $secondNumberText"
+        val firstFormattedNumber = if(firstNumberText.isNotEmpty()) decimalFormat.format(firstNumberText.toString().toBigDecimal()) else ""
+        val secondFormattedNumber = if(secondNumberText.isNotEmpty()) decimalFormat.format(secondNumberText.toString().toBigDecimal()) else ""
+
+        binding.equationTextView.text = "$firstFormattedNumber $operatorText $secondFormattedNumber"
     }
 }
