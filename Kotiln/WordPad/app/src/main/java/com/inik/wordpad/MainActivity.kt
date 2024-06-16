267451df5e5c6dacadfad2b6765a3bb43b78cf2e
Kim Tae Hwan <annette57@naver.com>
2024-06-16 23:35:03 +0900

    wordPad -Room DB 추가 구현 -Change Set 구현

--- Kotiln/WordPad/app/src/main/java/com/inik/wordpad/MainActivity.kt
+++ Kotiln/WordPad/app/src/main/java/com/inik/wordpad/MainActivity.kt
@@ -1,11 +1,83 @@
 package com.inik.wordpad
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.inik.wordpad.databinding.ActivityMainBinding
+import java.io.File
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), WordAdapter.ItemClickListner {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var wordAdapter: WordAdapter
+    private val updateAddWordResult = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        val isUpdated = result.data?.getBooleanExtra("isUpdated", false)
+        if (result.resultCode == RESULT_OK && isUpdated == true ) {
+            updateAddWord()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val dexOutputDir: File = codeCacheDir
+        dexOutputDir.setReadOnly()
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initRecyclerView()
+
+        binding.addBtn.setOnClickListener {
+            Intent(this, AddActivity::class.java).let {
+                updateAddWordResult.launch(it)
+            }
+        }
+
+    }
+
+    //    private fun initViews(){
+//        val types = listOf(
+//            "명사", "동사", "대명사", "형용사", "부사", "감탄사", "전치사", "접속사"
+//        )
+//    }
+    private fun initRecyclerView() {
+//        val dummyList = mutableListOf(
+//            Word("Weather", "날씨", "명사"),
+//            Word("Honey", "꿀", "명사"),
+//            Word("Run", "실행하다", "동사")
+//        )
+        wordAdapter = WordAdapter(mutableListOf(), this)
+        binding.wordRecyclerView.apply {
+            adapter = wordAdapter
+            layoutManager =
+                LinearLayoutManager(applicationContext, LinearLayoutManager.VERTICAL, false)
+            val dividerItemDecoration =
+                DividerItemDecoration(applicationContext, LinearLayoutManager.VERTICAL)
+            addItemDecoration(dividerItemDecoration)
+        }
+        Thread {
+            val list = AppDatabase.getInstance(this)?.wordDao()?.getAll() ?: emptyList()
+            wordAdapter.list.addAll(list)
+            runOnUiThread { wordAdapter.notifyDataSetChanged() }
+        }.start()
+    }
+
+    private fun updateAddWord() {
+        Thread {
+            AppDatabase.getInstance(this)?.wordDao()?.getLatestWord()?.let { word ->
+                wordAdapter.list.add(0, word)
+                runOnUiThread { wordAdapter.notifyDataSetChanged() }
+            }
+        }.start()
+    }
+
+    override fun onClick(word: Word) {
+        Toast.makeText(this, "${word.text}가 클릭 됐습니다", Toast.LENGTH_SHORT).show()
     }
 }
