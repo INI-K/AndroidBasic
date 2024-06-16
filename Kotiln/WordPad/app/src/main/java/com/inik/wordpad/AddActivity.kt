e5d9d3e1aef1d1c48606f539774de46b1579f139
INI-K_MAC_MINI <>
2024-06-16 23:35:03 +0900

    wordPad -Room DB 추가 구현 -Change Set 구현

---
+++ Kotiln/WordPad/app/src/main/java/com/inik/wordpad/AddActivity.kt
@@ -0,0 +1,56 @@
+package com.inik.wordpad
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.google.android.material.chip.Chip
+import com.inik.wordpad.databinding.ActivityAddActiviryBinding
+
+class AddActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAddActiviryBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAddActiviryBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initViews()
+        binding.addBtn.setOnClickListener {
+            add()
+        }
+    }
+
+    private fun initViews() {
+        val types = listOf("명사", "동사", "대명사", "형용사", "부사", "감탄사", "전치사", "접속사")
+        binding.typeChipGroup.apply {
+            types.forEach { text ->
+                addView(createChip(text))
+            }
+        }
+    }
+
+    private fun createChip(text: String): Chip {
+        return Chip(this).apply {
+            setText(text)
+            isCheckable = true
+            isClickable = true
+        }
+    }
+
+    private fun add() {
+        val text = binding.textInputEditText.text.toString()
+        val mean = binding.meanTextInputEditText.text.toString()
+        val type = findViewById<Chip>(binding.typeChipGroup.checkedChipId).text.toString()
+        val word = Word(text, mean, type)
+
+        Thread {
+            AppDatabase.getInstance(this)?.wordDao()?.insert(word)
+            runOnUiThread {
+                Toast.makeText(this, "저장을 완료했습니다.", Toast.LENGTH_LONG).show()
+            }
+            val intent = Intent().putExtra("isUpdated", true)
+            setResult(RESULT_OK, intent)
+            finish()
+        }.start()
+    }
+}
