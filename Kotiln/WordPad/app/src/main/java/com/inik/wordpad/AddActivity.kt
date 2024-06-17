68333434f5b53106185758430904f41f14c85b25
INI-K_MAC_MINI <>
2024-06-17 19:02:02 +0900

    WordPad - 단어장 추가,수정,삭제 - 오류처리

--- Kotiln/WordPad/app/src/main/java/com/inik/wordpad/AddActivity.kt
+++ Kotiln/WordPad/app/src/main/java/com/inik/wordpad/AddActivity.kt
@@ -4,11 +4,14 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.core.view.children
+import androidx.core.widget.addTextChangedListener
 import com.google.android.material.chip.Chip
 import com.inik.wordpad.databinding.ActivityAddActiviryBinding
 
 class AddActivity : AppCompatActivity() {
     private lateinit var binding: ActivityAddActiviryBinding
+    private var originWord : Word? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAddActiviryBinding.inflate(layoutInflater)
@@ -16,7 +19,7 @@ class AddActivity : AppCompatActivity() {
 
         initViews()
         binding.addBtn.setOnClickListener {
-            add()
+            if(originWord == null) add() else edit()
         }
     }
 
@@ -26,6 +29,25 @@ class AddActivity : AppCompatActivity() {
             types.forEach { text ->
                 addView(createChip(text))
             }
+        }
+
+        binding.textInputEditText.addTextChangedListener {
+            it?.let { text->
+               binding.textInputEditText.error =
+                   when(text.length){
+                    0 -> "값을 입력해주세요"
+                    1 -> "2글자 이상 입력해주세요"
+                    else -> null
+                }
+            }
+        }
+
+        originWord = intent.getParcelableExtra("originWord")
+        originWord?.let { word ->
+            binding.textInputEditText.setText(word.text)
+            binding.meanTextInputEditText.setText(word.text)
+            val selectChip = binding.typeChipGroup.children.firstOrNull {(it as Chip).text == word.type} as? Chip
+            selectChip?.isCheckable = true
         }
     }
 
@@ -51,6 +73,22 @@ class AddActivity : AppCompatActivity() {
             val intent = Intent().putExtra("isUpdated", true)
             setResult(RESULT_OK, intent)
             finish()
+        }.start()
+    }
+
+    private fun edit(){
+        val text = binding.textInputEditText.text.toString()
+        val mean = binding.meanTextInputEditText.text.toString()
+        val type = findViewById<Chip>(binding.typeChipGroup.checkedChipId).text.toString()
+        val editWord = originWord?.copy(text = text, mean = mean, type = type)
+        Thread{
+            editWord?.let { word ->
+                AppDatabase.getInstance(this)?.wordDao()?.update(editWord)
+                val intent = Intent().putExtra("editWord", editWord)
+                setResult(RESULT_OK,intent)
+                runOnUiThread { Toast.makeText(this,"수정을 완료 했습니다.", Toast.LENGTH_LONG).show() }
+                finish()
+            }
         }.start()
     }
 }
