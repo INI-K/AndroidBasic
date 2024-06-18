b9bc798750e47f09f6a597a182542c09fb375d41
Kim Tae Hwan <annette57@naver.com>
2024-06-18 22:28:56 +0900

    MyPhotoFrame -ToolBar 추가 -ToolBar 요소 구현(뒤로가기, 사진추가하기)

--- Kotiln/MyPhotoFrame/app/src/main/java/com/inik/myphotoframe/MainActivity.kt
+++ Kotiln/MyPhotoFrame/app/src/main/java/com/inik/myphotoframe/MainActivity.kt
@@ -6,6 +6,8 @@ import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
@@ -30,6 +32,11 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.toolbar.apply {
+            title = "사진 가져오기"
+            setSupportActionBar(this)
+        }
+
         binding.loadImageBtn.setOnClickListener {
             checkPermission()
         }
@@ -37,6 +44,23 @@ class MainActivity : AppCompatActivity() {
             navigateToFrameActivity()
         }
         initRecyclerView()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return  when(item.itemId){
+            R.id.action_add -> {
+                checkPermission()
+                true
+            }
+            else -> {
+                return super.onOptionsItemSelected(item)
+            }
+        }
     }
 
     private fun navigateToFrameActivity(){
