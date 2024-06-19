cfd98221aeceb90594a9d2fd0398d8eae28501e7
INI-K_MAC_MINI <>
2024-06-19 19:44:08 +0900

    MediaPlayer -재생로직 Service 구현

---
+++ Kotiln/MediaPlayer/app/src/main/java/com/inik/mediaplayer/Constants.kt
@@ -0,0 +1,6 @@
+package com.inik.mediaplayer
+
+const val MEDIA_PLAYER_PLAY = "play"
+const val MEDIA_PLAYER_PAUSE = "pause"
+const val MEDIA_PLAYER_STOP = "stop"
+const val CHANNEL_ID = "MEDIA_PLAY_CHANNEL"
