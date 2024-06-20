31eb8c1748464f763ceca2f885281546aebfca09
Kim Tae Hwan <annette57@naver.com>
2024-06-20 21:32:40 +0900

    WebToon - 탭 이름 바꾸기구현 - 완성

--- Kotiln/WebToon/app/src/main/java/com/inik/webtoon/MainActivity.kt
+++ Kotiln/WebToon/app/src/main/java/com/inik/webtoon/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.inik.webtoon
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.Gravity
 import android.webkit.WebView
 import android.webkit.WebViewClient
@@ -12,10 +14,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.tabs.TabLayoutMediator
+import com.inik.webtoon.WebViewFragment.Companion.SHARED_PREFERENCES
 import com.inik.webtoon.databinding.ActivityMainBinding
 import com.inik.webtoon.databinding.FragmentWebviewBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),OnTabLayoutNameChanged {
 
     private lateinit var binding: ActivityMainBinding
 
@@ -24,18 +27,26 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val sharedPreferences = getSharedPreferences(WebViewFragment.Companion.SHARED_PREFERENCES,Context.MODE_PRIVATE)
+        val tab0 = sharedPreferences.getString("tab0_name","월요 웹툰")
+        val tab1 = sharedPreferences.getString("tab1_name","화요 웹툰")
+        val tab2 = sharedPreferences.getString("tab2_name","수요 웹툰")
 
 
         binding.viewPager.adapter = ViewPagerAdapter(this)
 
         TabLayoutMediator(binding.tabLayout,binding.viewPager){ tab, position ->
             run {
-                val textView = TextView(this@MainActivity)
-                textView.text = "position $position"
-                textView.gravity = Gravity.CENTER
-
-                tab.customView = textView
-//                tab.text = "position $position"
+//                val textView = TextView(this@MainActivity)
+//                textView.text = "position $position"
+//                textView.gravity = Gravity.CENTER
+//
+//                tab.customView = textView
+                tab.text = when(position){
+                    0 -> { tab0 }
+                    1 -> { tab1 }
+                    else -> { tab2 }
+                }
             }
         }.attach()
     }
@@ -52,5 +63,11 @@ class MainActivity : AppCompatActivity() {
             super.onBackPressed()
         }
 //        super.onBackPressed()
+    }
+
+    override fun nameChanged(position: Int, name: String) {
+        Log.d("실행 확인 " , "번호 : $position")
+        val tab = binding.tabLayout.getTabAt(position)
+        tab?.text = name
     }
 }
--- Kotiln/WebToon/app/src/main/java/com/inik/webtoon/WebViewFragment.kt
+++ Kotiln/WebToon/app/src/main/java/com/inik/webtoon/WebViewFragment.kt
@@ -6,13 +6,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.webkit.WebViewClient
+import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.core.content.edit
 import androidx.fragment.app.Fragment
 import com.inik.webtoon.databinding.FragmentWebviewBinding
 
-class WebViewFragment(private val position: Int): Fragment() {
+class WebViewFragment(private val position: Int, private val webViewUrl: String): Fragment() {
     private lateinit var binding: FragmentWebviewBinding
+    var listener: OnTabLayoutNameChanged? = null
+    companion object{
+        const val SHARED_PREFERENCES = "WEB_HISTORY"
+    }
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -26,23 +32,39 @@ class WebViewFragment(private val position: Int): Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.webView.webViewClient = WebtoonWebClient(binding.webViewProgressBar) {url ->
-            activity?.getSharedPreferences("WEB_HISTORY",Context.MODE_PRIVATE)?.edit {
+            activity?.getSharedPreferences(SHARED_PREFERENCES,Context.MODE_PRIVATE)?.edit {
                 putString("tab$position", url)
                 commit()
             }
         }
 
         binding.webView.settings.javaScriptEnabled = true
-        binding.webView.loadUrl("https://comic.naver.com/")
+        binding.webView.loadUrl(webViewUrl)
 
         binding.backToLastBtn.setOnClickListener {
-            val sharedPreferences = activity?.getSharedPreferences("WEB_HISTORY",Context.MODE_PRIVATE)
+            val sharedPreferences = activity?.getSharedPreferences(SHARED_PREFERENCES,Context.MODE_PRIVATE)
             val url = sharedPreferences?.getString("tab$position","")
             if(url.isNullOrEmpty()){
                 Toast.makeText(context,"마지막 저장 시점이 없습니다.",Toast.LENGTH_SHORT).show()
             }else{
                 binding.webView.loadUrl(url)
             }
+        }
+
+        binding.changeTabNameBtn.setOnClickListener {
+            val dialog = context?.let { context -> AlertDialog.Builder(context) }
+            val editText = EditText(context)
+
+            dialog?.setView(editText)
+            dialog?.setPositiveButton("저장"){_,_ ->
+                activity?.getSharedPreferences(SHARED_PREFERENCES,Context.MODE_PRIVATE)?.edit {
+                    putString("tab${position}_name", editText.text.toString())
+                    listener?.nameChanged(position,editText.text.toString())
+                }
+            }
+            dialog?.setNegativeButton("취소"){dialogInterface,_ ->
+                dialogInterface.cancel()
+            }?.show()
         }
     }
 
@@ -53,5 +75,8 @@ class WebViewFragment(private val position: Int): Fragment() {
     fun canGoback(): Boolean{
         return binding.webView.canGoBack()
     }
+}
 
+interface OnTabLayoutNameChanged{
+    fun nameChanged(position: Int, name: String)
 }
