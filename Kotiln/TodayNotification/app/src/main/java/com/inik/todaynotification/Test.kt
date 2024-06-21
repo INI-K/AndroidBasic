7d442bc99f4fe63fa4d15b326599b1601077c9f9
Kim Tae Hwan <annette57@naver.com>
2024-06-21 22:30:23 +0900

    TodayNoti -First Init -소켓 예시 -OkHttp -Gson -로컬 서버(Kotlin)

---
+++ Kotiln/TodayNotification/app/src/main/java/com/inik/todaynotification/Test.kt
@@ -0,0 +1,46 @@
+package com.inik.todaynotification
+
+import android.util.Log
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.io.PrintWriter
+import java.net.ServerSocket
+
+fun main(){
+    Thread{
+        val port = 8080
+        val server = ServerSocket(port)
+
+        while (true){
+            val socket = server.accept()
+
+            socket.getInputStream()
+            socket.getOutputStream()
+
+            val reader = BufferedReader(InputStreamReader(socket.getInputStream()))
+            val printer = PrintWriter(socket.getOutputStream())
+
+            var input: String? = "-1"
+            while (input != null && input != ""){
+                input = reader.readLine()
+            }
+
+//        Log.e("SERVER", "READ DATA $input")
+
+            System.out.println("READ DATA $input")
+
+            printer.println("HTTP/1.1 200 OK")
+            printer.println("Content-Type: text/html\r\n")
+
+            printer.println("{\"message\": \"Hello World\"}")
+            printer.println("\r\n")
+            printer.flush()
+            printer.close()
+
+            reader.close()
+
+            socket.close()
+        }
+    }.start()
+
+}
