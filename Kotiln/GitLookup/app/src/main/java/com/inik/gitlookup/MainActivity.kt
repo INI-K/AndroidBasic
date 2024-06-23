761f54a5dd47405aa7a82f9de6b4c57a26e53ee9
Kim Tae Hwan <annette57@naver.com>
2024-06-23 21:07:38 +0900

    GitLookup -어답터 구현 -리사이클러뷰 구현 -통신api구현

--- Kotiln/GitLookup/app/src/main/java/com/inik/gitlookup/MainActivity.kt
+++ Kotiln/GitLookup/app/src/main/java/com/inik/gitlookup/MainActivity.kt
@@ -6,7 +6,11 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.inik.gitlookup.adapter.UserAdapter
+import com.inik.gitlookup.databinding.ActivityMainBinding
 import com.inik.gitlookup.model.Repo
+import com.inik.gitlookup.model.UserDto
 import com.inik.gitlookup.network.GIthubService
 import retrofit2.Call
 import retrofit2.Callback
@@ -15,19 +19,19 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val retrofit = Retrofit.Builder()
-            .baseUrl("https://api.github.com/")
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
+        val retrofit = Retrofit.Builder().baseUrl("https://api.github.com/")
+            .addConverterFactory(GsonConverterFactory.create()).build()
 
         val gIthubService = retrofit.create(GIthubService::class.java)
-        gIthubService.listRepos("square").enqueue(object: Callback<List<Repo>> {
+        gIthubService.listRepos("square").enqueue(object : Callback<List<Repo>> {
             override fun onResponse(call: Call<List<Repo>>, response: Response<List<Repo>>) {
-               Log.e("MainActivity", response.body().toString())
+                Log.e("MainActivity", response.body().toString())
             }
 
             override fun onFailure(call: Call<List<Repo>>, t: Throwable) {
@@ -35,5 +39,27 @@ class MainActivity : AppCompatActivity() {
             }
 
         })
+
+
+        val userAdapter = UserAdapter()
+
+        binding.userRecyclerView.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = userAdapter
+        }
+
+        gIthubService.searchUsers("squar").enqueue(object : Callback<UserDto> {
+            override fun onResponse(call: Call<UserDto>, response: Response<UserDto>) {
+                Log.e("유저 검색", "유저 :  ${response.body().toString()}")
+
+                userAdapter.submitList(response.body()?.items)
+            }
+
+            override fun onFailure(call: Call<UserDto>, t: Throwable) {
+
+            }
+
+        })
+
     }
 }
