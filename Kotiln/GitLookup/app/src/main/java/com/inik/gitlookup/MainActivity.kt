074d3f3708b318f766fe320de08eae1e3f440132
INI-K_MAC_MINI <>
2024-06-23 21:27:25 +0900

    GItLookUp -레포액티비티 생성 -어답터 클릭 리스너 구현

--- Kotiln/GitLookup/app/src/main/java/com/inik/gitlookup/MainActivity.kt
+++ Kotiln/GitLookup/app/src/main/java/com/inik/gitlookup/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.inik.gitlookup
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.widget.addTextChangedListener
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.inik.gitlookup.adapter.UserAdapter
 import com.inik.gitlookup.databinding.ActivityMainBinding
@@ -17,38 +22,59 @@ import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.Query
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var gIthubService: GIthubService
+    private lateinit var userAdapter: UserAdapter
+    private var retrofit =  Retrofit.Builder().baseUrl("https://api.github.com/")
+    .addConverterFactory(GsonConverterFactory.create()).build()
+
+    private var searchFor: String = ""
+    private val handler = Handler(Looper.getMainLooper())
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val retrofit = Retrofit.Builder().baseUrl("https://api.github.com/")
-            .addConverterFactory(GsonConverterFactory.create()).build()
+        gIthubService = retrofit.create(GIthubService::class.java)
 
-        val gIthubService = retrofit.create(GIthubService::class.java)
-        gIthubService.listRepos("square").enqueue(object : Callback<List<Repo>> {
-            override fun onResponse(call: Call<List<Repo>>, response: Response<List<Repo>>) {
-                Log.e("MainActivity", response.body().toString())
-            }
+//        gIthubService.listRepos("square").enqueue(object : Callback<List<Repo>> {
+//            override fun onResponse(call: Call<List<Repo>>, response: Response<List<Repo>>) {
+//                Log.e("MainActivity", response.body().toString())
+//            }
+//
+//            override fun onFailure(call: Call<List<Repo>>, t: Throwable) {
+//
+//            }
+//
+//        })
 
-            override fun onFailure(call: Call<List<Repo>>, t: Throwable) {
-
-            }
-
-        })
-
-
-        val userAdapter = UserAdapter()
+        userAdapter = UserAdapter{
+            val intent = Intent(this@MainActivity, RepoActivity::class.java)
+            intent.putExtra("username",it.userName)
+            startActivity(intent)
+        }
 
         binding.userRecyclerView.apply {
             layoutManager = LinearLayoutManager(context)
             adapter = userAdapter
         }
 
-        gIthubService.searchUsers("squar").enqueue(object : Callback<UserDto> {
+        val runnable = Runnable {
+            searchUser()
+        }
+
+        binding.searchEditText.addTextChangedListener {
+            searchFor = it.toString()
+            handler.removeCallbacks(runnable)
+            handler.postDelayed(runnable,300)
+        }
+    }
+
+    private fun searchUser() {
+        gIthubService.searchUsers(searchFor).enqueue(object : Callback<UserDto> {
             override fun onResponse(call: Call<UserDto>, response: Response<UserDto>) {
                 Log.e("유저 검색", "유저 :  ${response.body().toString()}")
 
@@ -56,10 +82,10 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onFailure(call: Call<UserDto>, t: Throwable) {
-
+                Toast.makeText(this@MainActivity, "에러가 발생했읍니다이",Toast.LENGTH_SHORT).show()
+                t.printStackTrace()
             }
 
         })
-
     }
 }
