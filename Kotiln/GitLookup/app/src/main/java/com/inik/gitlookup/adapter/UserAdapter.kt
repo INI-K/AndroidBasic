70d5c0bfb3f0b99b777d969d2af246b114f8da8d
INI-K_MAC_MINI <>
2024-06-23 21:07:38 +0900

    GitLookup -어답터 구현 -리사이클러뷰 구현 -통신api구현

---
+++ Kotiln/GitLookup/app/src/main/java/com/inik/gitlookup/adapter/UserAdapter.kt
@@ -0,0 +1,47 @@
+package com.inik.gitlookup.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.inik.gitlookup.databinding.ItemUserBinding
+import com.inik.gitlookup.model.User
+
+class UserAdapter : ListAdapter<User, UserAdapter.ViewHolder>(diffUtil) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            ItemUserBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val user = currentList[position]
+        holder.bind(user)
+    }
+
+    inner class ViewHolder(private var viewBinding: ItemUserBinding) :
+        RecyclerView.ViewHolder(viewBinding.root) {
+        fun bind(item: User) {
+            viewBinding.usernameTextView.text = item.userName
+        }
+    }
+
+    companion object {
+        val diffUtil = object : DiffUtil.ItemCallback<User>() {
+            override fun areItemsTheSame(oldItem: User, newItem: User): Boolean {
+                return oldItem.id == newItem.id
+            }
+
+            override fun areContentsTheSame(oldItem: User, newItem: User): Boolean {
+                return oldItem == newItem
+            }
+
+        }
+    }
+}
