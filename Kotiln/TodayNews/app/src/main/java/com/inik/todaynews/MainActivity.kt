bf1cf433f069cbc03f73e4bfcc74d0562b61178d
INI-K_MAC_MINI <>
2024-06-25 20:19:55 +0900

    TodayNews -리사이클러뷰 작성

--- Kotiln/TodayNews/app/src/main/java/com/inik/todaynews/MainActivity.kt
+++ Kotiln/TodayNews/app/src/main/java/com/inik/todaynews/MainActivity.kt
@@ -6,6 +6,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.inik.todaynews.databinding.ActivityMainBinding
 import com.tickaroo.tikxml.TikXml
 import com.tickaroo.tikxml.TikXmlConfig
 import com.tickaroo.tikxml.retrofit.TikXmlConverterFactory
@@ -15,6 +17,8 @@ import retrofit2.Response
 import retrofit2.Retrofit
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var newsAdapter: NewsAdapter
     private val retrofit = Retrofit.Builder()
         .baseUrl("https://news.google.com/")
         .addConverterFactory(
@@ -26,12 +30,21 @@ class MainActivity : AppCompatActivity() {
         ).build()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        newsAdapter = NewsAdapter()
+
+        binding.newsRecyclerView.apply {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            adapter = newsAdapter
+        }
 
         val newsService = retrofit.create(NewsService::class.java)
         newsService.mainFeed().enqueue(object : Callback<NewsRss>{
             override fun onResponse(call: Call<NewsRss>, response: Response<NewsRss>) {
                 Log.e("통신 성공"," ${response.body()?.channel?.items}")
+
+                newsAdapter.submitList(response.body()?.channel?.items.orEmpty())
             }
 
             override fun onFailure(call: Call<NewsRss>, t: Throwable) {
