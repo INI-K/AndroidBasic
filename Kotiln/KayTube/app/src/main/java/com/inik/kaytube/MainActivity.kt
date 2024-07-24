ec2443e8a14d22dc57ab4924e7be17965b748eef
Kim Tae Hwan <annette57@naver.com>
2024-07-24 22:58:23 +0900

    KayTube -first Init -모션레이아웃 지정 -메인 UI -Video DTO

---
+++ Kotiln/KayTube/app/src/main/java/com/inik/kaytube/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.inik.kaytube
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.inik.kaytube.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    private lateinit var videoAdapter: VideoAdapter
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        videoAdapter = VideoAdapter(context = this, onClick = {videoItem ->
+            binding.motionLayout.transitionToEnd()
+        })
+
+        binding.motionLayout.jumpToState(R.id.collapse)
+
+        binding.videoListRecyclerView.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = videoAdapter
+        }
+
+        val videoList = readData("videos.json",VideoList::class.java) ?: VideoList(emptyList())
+        videoAdapter.submitList(videoList.videos)
+
+    }
+}
