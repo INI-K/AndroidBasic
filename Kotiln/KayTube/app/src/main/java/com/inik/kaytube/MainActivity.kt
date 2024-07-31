7222a6408d6ce3229ab5121938e799a77cea51e3
Kim Tae Hwan <annette57@naver.com>
2024-07-31 17:28:50 +0900

    케이튜브 -video Container-터치 이벤트 구현 -View 스와이프 -모션레이아웃 커스텀

--- Kotiln/KayTube/app/src/main/java/com/inik/kaytube/MainActivity.kt
+++ Kotiln/KayTube/app/src/main/java/com/inik/kaytube/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.inik.kaytube
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
 import com.inik.kaytube.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,23 +17,74 @@ class MainActivity : AppCompatActivity() {
     }
 
     private lateinit var videoAdapter: VideoAdapter
+    private var player: ExoPlayer? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        videoAdapter = VideoAdapter(context = this, onClick = {videoItem ->
-            binding.motionLayout.transitionToEnd()
-        })
 
-        binding.motionLayout.jumpToState(R.id.collapse)
+
+//        binding.motionLayout.jumpToState(R.id.collapse)
+
+        initMotionLayout()
+
+        initVideoRecyclerView()
+
+    }
+
+    private fun initVideoRecyclerView() {
+        videoAdapter = VideoAdapter(context = this, onClick = { videoItem ->
+            binding.motionLayout.transitionToEnd()
+            play(videoItem)
+        })
 
         binding.videoListRecyclerView.apply {
             layoutManager = LinearLayoutManager(context)
             adapter = videoAdapter
         }
 
-        val videoList = readData("videos.json",VideoList::class.java) ?: VideoList(emptyList())
+        val videoList = readData("videos.json", VideoList::class.java) ?: VideoList(emptyList())
         videoAdapter.submitList(videoList.videos)
+    }
 
+    private fun initMotionLayout() {
+        binding.motionLayout.targetView = binding.videoPlayerContainer
+    }
+
+    private fun initExoPlayer() {
+        player = ExoPlayer.Builder(this).build()
+            .also {
+                binding.playerView.player = it
+            }
+    }
+
+    private fun play(videoItem: VideoItem){
+        player?.setMediaItem(MediaItem.fromUri(Uri.parse(videoItem.videoUrl)))
+        player?.prepare()
+        player?.play()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        player?.pause()
+    }
+    override fun onStart() {
+        super.onStart()
+        if (player == null) {
+            initExoPlayer()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (player == null) {
+            initExoPlayer()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        player?.release()
     }
 }
