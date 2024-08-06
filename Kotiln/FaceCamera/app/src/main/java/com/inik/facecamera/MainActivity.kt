0c4e0d22f6833b260f67dae1f70be0cd26915076
INI-K <>
2024-08-06 18:41:24 +0900

    FaceCamera - 완 - 카메라화면 인식 - 카메라 얼굴 인식 프로그래스

--- Kotiln/FaceCamera/app/src/main/java/com/inik/facecamera/MainActivity.kt
+++ Kotiln/FaceCamera/app/src/main/java/com/inik/facecamera/MainActivity.kt
@@ -14,23 +14,24 @@ import com.inik.face_recognition.Camera
 import com.inik.face_recognition.recognition.FaceAnalyzerListener
 import com.inik.facecamera.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity(),FaceAnalyzerListener {
+class MainActivity : AppCompatActivity(), FaceAnalyzerListener {
     private lateinit var binding: ActivityMainBinding
     private val camera = Camera(this)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater).apply {
-            setContentView(root)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-            setProgressText("시작하기를 눌러주세요")
-            camera.initCamera(binding.cameraLayout, this@MainActivity)
+        setProgressText("시작하기를 눌러주세요")
+        camera.initCamera(binding.cameraLayout, this@MainActivity)
 
-            startDetectBtn.setOnClickListener{
-                it.isVisible = false
-                camera.startFaceDetect()
-                setProgressText("얼굴을 보여주세요")
-            }
+        binding.startDetectBtn.setOnClickListener {
+            it.isVisible = false
+            binding.faceOverlayView.reset()
+            camera.startFaceDetect()
+            setProgressText("얼굴을 보여주세요")
         }
+
     }
 
     override fun detect() {
@@ -43,15 +44,16 @@ class MainActivity : AppCompatActivity(),FaceAnalyzerListener {
     }
 
     override fun notDetect() {
-
+        binding.faceOverlayView.reset()
     }
 
     override fun detectProgress(progess: Float, message: String) {
-       setProgressText(message)
+        setProgressText(message)
+        binding.faceOverlayView.setProgress(progess)
     }
 
     override fun faceSize(rectF: RectF, sizeF: SizeF, pointF: PointF) {
-
+        binding.faceOverlayView.setSize(rectF,sizeF, pointF)
     }
 
     override fun onRequestPermissionsResult(
@@ -63,10 +65,11 @@ class MainActivity : AppCompatActivity(),FaceAnalyzerListener {
         camera.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 
-    private fun reset(){
-        binding.startDetectBtn.isVisible =true
+    private fun reset() {
+        binding.startDetectBtn.isVisible = true
     }
-    private fun setProgressText(text: String){
+
+    private fun setProgressText(text: String) {
         TransitionManager.beginDelayedTransition(binding.root)
         binding.progressTextView.text = text
     }
