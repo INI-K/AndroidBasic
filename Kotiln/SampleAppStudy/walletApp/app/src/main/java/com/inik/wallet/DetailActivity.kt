d9c868c67462afe7768f5a53d117aaee4219ac17
annette57@naver.com <annette57@naver.com>
2024-09-12 14:39:01 +0900

    Wallet -카드사용 히스토리 구현 -Complete

--- Kotiln/SampleAppStudy/walletApp/app/src/main/java/com/inik/wallet/DetailActivity.kt
+++ Kotiln/SampleAppStudy/walletApp/app/src/main/java/com/inik/wallet/DetailActivity.kt
@@ -7,6 +7,10 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityOptionsCompat
 import com.inik.wallet.databinding.ActivityDetailBinding
+import com.inik.wallet.model.DetailItem
+import com.inik.wallet.model.Type
+import java.time.Year
+import java.util.Calendar
 
 class DetailActivity: AppCompatActivity() {
     private lateinit var binding: ActivityDetailBinding
@@ -17,12 +21,77 @@ class DetailActivity: AppCompatActivity() {
         binding = ActivityDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
         initView()
+
+        adapter.submitList(mockData())
     }
 
     private fun initView(){
         binding.cardTitleTextView.text = intent.getStringExtra(CARD_NAME)
         binding.cardLayout.backgroundTintList = intent.getParcelableExtra(CARD_COLOR) as? ColorStateList
         binding.recyclerView.adapter = adapter
+    }
+
+    private fun mockData(): List<DetailItem>{
+        fun createDate(year: Int, month: Int, day:Int) = Calendar.getInstance().apply {
+            set(year,month,day)
+        }.time
+        val list = mutableListOf<DetailItem>().apply {
+            add(
+                DetailItem(
+                1,
+                createDate(2032,1,6 ),
+                "상점1",
+                25100,
+                Type.PAY
+            )
+            )
+            add(
+                DetailItem(
+                    2,
+                    createDate(2032,1,6 ),
+                    "상점2",
+                    253400,
+                    Type.PAY
+                )
+            )
+            add(
+                DetailItem(
+                    3,
+                    createDate(2032,1,6 ),
+                    "상점2",
+                    253400,
+                    Type.CANCEL
+                )
+            )
+            add(
+                DetailItem(
+                    4,
+                    createDate(2032,1,6 ),
+                    "상점 23",
+                    253400,
+                    Type.PAY
+                )
+            )
+            add(
+                DetailItem(
+                    5,
+                    createDate(2032,1,6 ),
+                    "상점5",
+                    253400,
+                    Type.CANCEL
+                )
+            )
+            add(
+                DetailItem(
+                    6,
+                    createDate(2032,1,6 ),
+                    "상점234",
+                    253400,
+                    Type.POINT
+                )
+            )
+        }
+        return list
     }
 
     companion object{
