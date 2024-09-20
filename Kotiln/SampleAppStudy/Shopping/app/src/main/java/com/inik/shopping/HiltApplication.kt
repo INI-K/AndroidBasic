fbc96d93b32507b1a1d650eb0f7190c6c23877d3
annette57@naver.com <annette57@naver.com>
2024-09-20 09:55:51 +0900

    Shopping -이미지 수정 -앱바 UI구현

--- Kotiln/SampleAppStudy/Shopping/app/src/main/java/com/inik/shopping/HiltApplication.kt
+++ Kotiln/SampleAppStudy/Shopping/app/src/main/java/com/inik/shopping/HiltApplication.kt
@@ -1,8 +1,13 @@
 package com.inik.shopping
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class HiltApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+    }
 }
--- Kotiln/SampleAppStudy/Shopping/app/src/main/java/com/inik/shopping/MainActivity.kt
+++ Kotiln/SampleAppStudy/Shopping/app/src/main/java/com/inik/shopping/MainActivity.kt
@@ -5,12 +5,18 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.inik.shopping.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater).apply {
+            setContentView(root)
+        }
     }
 }
