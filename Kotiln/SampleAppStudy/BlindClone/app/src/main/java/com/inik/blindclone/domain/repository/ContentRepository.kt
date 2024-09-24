212c86badf1c7800bd95e833e2fff9f8cf907b52
annette57@naver.com <annette57@naver.com>
2024-09-24 14:38:26 +0900

    BlindClone -수정

--- Kotiln/SampleAppStudy/BlindClone/app/src/main/java/com/inik/blindclone/domain/repository/ContentRepository.kt
+++ Kotiln/SampleAppStudy/BlindClone/app/src/main/java/com/inik/blindclone/domain/repository/ContentRepository.kt
@@ -5,4 +5,5 @@ import com.inik.blindclone.domain.model.Content
 interface ContentRepository {
     suspend fun save(item: Content):Boolean
 
+    suspend fun update(item: Content):Boolean
 }
--- Kotiln/SampleAppStudy/BlindClone/app/src/main/java/com/inik/blindclone/presenter/viewmodel/InputViewModel.kt
+++ Kotiln/SampleAppStudy/BlindClone/app/src/main/java/com/inik/blindclone/presenter/viewmodel/InputViewModel.kt
@@ -3,12 +3,19 @@ package com.inik.blindclone.presenter.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.inik.blindclone.domain.model.Content
+import com.inik.blindclone.domain.usecase.ContentUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import retrofit2.http.Path
+import javax.inject.Inject
 
 @HiltViewModel
-class InputViewModel : ViewModel() {
+class InputViewModel @Inject constructor(
+    private val contentUseCase: ContentUseCase
+) : ViewModel() {
     private val _doneEvent = MutableLiveData<Pair<Boolean, String>>()
 
     val doneEvent: LiveData<Pair<Boolean, String>> = _doneEvent
@@ -33,9 +40,19 @@ class InputViewModel : ViewModel() {
             titleValue.isNullOrBlank() ||
             contentValue.isNullOrBlank()
         ) {
-            _doneEvent.value = Pair(false,"모든 항목을 입력하셔야 합니다.")
+            _doneEvent.value = Pair(false, "모든 항목을 입력하셔야 합니다.")
+            return
+        }
+        viewModelScope.launch(Dispatchers.IO) {
+            contentUseCase.save(
+                item?.copy(
+                    category = categoryValue,
+                    title = titleValue,
+                    content = contentValue
+                ) ?: Content(category = categoryValue, title = titleValue, content = contentValue)
+            ).also {
+                _doneEvent.postValue(Pair(true, if (it as Boolean) "완료!" else "저장 할 수 없습니다."))
+            }
         }
     }
-
-    //todo api
 }
