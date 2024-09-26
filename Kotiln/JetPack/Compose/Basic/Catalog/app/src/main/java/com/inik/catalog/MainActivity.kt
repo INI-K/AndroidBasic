ce418f71cb5c87866913b60bf4f690ec9fb4333e
annette57@naver.com <annette57@naver.com>
2024-09-26 10:27:39 +0900

    Compose -scaffold -Catalog(List)

---
+++ Kotiln/JetPack/Compose/Basic/Catalog/app/src/main/java/com/inik/catalog/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.inik.catalog
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.inik.catalog.ui.theme.CatalogTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CatalogTheme {
+                Greeting(
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting() {
+    Text()
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    CatalogTheme {
+        Greeting()
+    }
+}
---
+++ Kotiln/JetPack/Compose/Basic/Scaffold/app/src/main/java/com/inik/scaffold/MainActivity.kt
@@ -0,0 +1,101 @@
+package com.inik.scaffold
+
+import android.annotation.SuppressLint
+import android.graphics.drawable.Icon
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.inik.scaffold.ui.theme.ScaffoldTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ScaffoldTheme {
+                Greeting()
+            }
+        }
+    }
+}
+
+@Composable
+fun CheckBoxWithContent(
+    checked: Boolean,
+    toggleState: () -> Unit,
+    content: @Composable RowScope.() -> Unit
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.clickable { toggleState() }
+    ) {
+        Checkbox(
+            checked = checked,
+            onCheckedChange = { toggleState() }
+        )
+        content()
+    }
+}
+
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Greeting() {
+    var checked by remember { mutableStateOf(false) }
+    Scaffold(topBar = {
+        TopAppBar(
+            navigationIcon = {
+                IconButton(onClick = {}) {
+                    Image(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = "뒤로가기"
+                    )
+                }
+            },
+            title = {
+                Text(text = "Scaffold App")
+            }
+        )
+    }) {
+        Surface(modifier = Modifier.padding(8.dp)) {
+            CheckBoxWithContent(checked = checked, toggleState = { checked = !checked }) {
+                Text(text = "컴포즈 Good")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ScaffoldTheme {
+        Greeting()
+    }
+}
