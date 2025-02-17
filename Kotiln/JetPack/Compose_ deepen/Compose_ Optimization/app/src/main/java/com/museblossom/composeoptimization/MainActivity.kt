3c59c4bb5cdcf9c4b46ffe2a198f5c4653b81514
INI-K <annette57@naver.com>
2025-02-17 18:19:35 +0900

    jetpack

--- Kotiln/JetPack/Compose_ deepen/Compose_ Optimization/app/src/main/java/com/museblossom/composeoptimization/MainActivity.kt
+++ Kotiln/JetPack/Compose_ deepen/Compose_ Optimization/app/src/main/java/com/museblossom/composeoptimization/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.museblossom.composeoptimization
 
-import android.os.Bundle
+
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
